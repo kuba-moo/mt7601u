e53452048a74197bc8667d78bb7f56625b072c5a
Jakub Kicinski <kubakici@wp.pl>
2015-02-15 20:55:02 +0100

    initialize avg_ampdu_len
    Signed-off-by: Jakub Kicinski <kubakici@wp.pl>

--- init.c
+++ init.c
@@ -771,6 +771,7 @@ struct mt7601u_dev *mt7601u_alloc_device(struct device *pdev)
 	spin_lock_init(&dev->rx_lock);
 	spin_lock_init(&dev->lock);
 	spin_lock_init(&dev->last_beacon.lock);
+	atomic_set(&dev->avg_ampdu_len, 1);
 
 	dev->stat_wq = alloc_workqueue("mt7601u", WQ_UNBOUND, 0);
 	if (!dev->stat_wq) {
