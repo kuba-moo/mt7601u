456b0972f8aec35b745fc3a76bc7c78a77e81e9c
Jakub Kicinski <kubakici@wp.pl>
2015-03-31 19:50:01 +0200

    remove comment about RTS threshold
    There are no plans to support AMSDU transmission.

--- main.c
+++ main.c
@@ -373,9 +373,6 @@ static int mt7601u_set_rts_threshold(struct ieee80211_hw *hw, u32 value)
 {
 	struct mt7601u_dev *dev = hw->priv;
 
-	printk("%s\n", __func__);
-
-	/* TODO: if AMSDU is possible set to 0x1000 (see AsicUpdateProtect()) */
 	mt76_rmw_field(dev, MT_TX_RTS_CFG, MT_TX_RTS_CFG_THRESH, value);
 
 	return 0;
